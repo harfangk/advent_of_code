333351b054e6479379120d3c85e6890d06dfcfd5
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-12-06 01:41:27 +0900

    Day 1 part 2 solution
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- app/Year2023/Day01/Solution.hs
+++ app/Year2023/Day01/Solution.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Year2023.Day01.Solution (solve) where
 
 import Data.Char (digitToInt, isDigit)
+import Data.List (stripPrefix)
 import Paths_advent_of_code (getDataFileName)
 
 input :: String
@@ -10,13 +13,50 @@ solve :: IO ()
 solve = do
   filename <- getDataFileName input
   file <- readFile filename
-  let part1Result = sum . map getCalibrationValue1 . lines $ file
+  let part1Result = sum . map (getCalibrationValue . getDigits) . lines $ file
   print part1Result
+  let part2Result = sum . map getDigits2 . lines $ file
+  print part2Result
 
-getCalibrationValue1 :: String -> Int
-getCalibrationValue1 text =
-  let digitText = map digitToInt . filter isDigit $ text
-   in case digitText of
-        [] -> 0
-        [n] -> n * 10 + n
-        (n : ns) -> n * 10 + last ns
+getDigits :: String -> [Int]
+getDigits = map digitToInt . filter isDigit
+
+getCalibrationValue :: [Int] -> Int
+getCalibrationValue [] = 0
+getCalibrationValue [n] = n * 10 + n
+getCalibrationValue (n : ns) = n * 10 + last ns
+
+getDigits2 :: String -> Int
+getDigits2 text = getFirstDigit text * 10 + (getSecondDigit . reverse $ text)
+  where
+    getFirstDigit :: String -> Int
+    getFirstDigit [] = 0
+    getFirstDigit (stripPrefix "one" -> Just _rest) = 1
+    getFirstDigit (stripPrefix "two" -> Just _rest) = 2
+    getFirstDigit (stripPrefix "three" -> Just _rest) = 3
+    getFirstDigit (stripPrefix "four" -> Just _rest) = 4
+    getFirstDigit (stripPrefix "five" -> Just _rest) = 5
+    getFirstDigit (stripPrefix "six" -> Just _rest) = 6
+    getFirstDigit (stripPrefix "seven" -> Just _rest) = 7
+    getFirstDigit (stripPrefix "eight" -> Just _rest) = 8
+    getFirstDigit (stripPrefix "nine" -> Just _rest) = 9
+    getFirstDigit (c : cs) =
+      if isDigit c
+        then digitToInt c
+        else getFirstDigit cs
+
+    getSecondDigit :: String -> Int
+    getSecondDigit [] = 0
+    getSecondDigit (stripPrefix "eno" -> Just _rest) = 1
+    getSecondDigit (stripPrefix "owt" -> Just _rest) = 2
+    getSecondDigit (stripPrefix "eerht" -> Just _rest) = 3
+    getSecondDigit (stripPrefix "ruof" -> Just _rest) = 4
+    getSecondDigit (stripPrefix "evif" -> Just _rest) = 5
+    getSecondDigit (stripPrefix "xis" -> Just _rest) = 6
+    getSecondDigit (stripPrefix "neves" -> Just _rest) = 7
+    getSecondDigit (stripPrefix "thgie" -> Just _rest) = 8
+    getSecondDigit (stripPrefix "enin" -> Just _rest) = 9
+    getSecondDigit (c : cs) =
+      if isDigit c
+        then digitToInt c
+        else getSecondDigit cs
