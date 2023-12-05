52e8d87459744c5c7be0b1a10de2cd22a2b288ee
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-12-06 00:38:28 +0900

    Remove usage of Data.Text
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- app/Year2023/Day01/Solution.hs
+++ app/Year2023/Day01/Solution.hs
@@ -1,8 +1,6 @@
 module Year2023.Day01.Solution (solve) where
 
-import Data.Char (isDigit)
-import Data.Text qualified as T
-import Data.Text.IO qualified as TIO
+import Data.Char (digitToInt, isDigit)
 import Paths_advent_of_code (getDataFileName)
 
 input :: String
@@ -11,14 +9,14 @@ input = "app/data/year2023/day01.txt"
 solve :: IO ()
 solve = do
   filename <- getDataFileName input
-  file <- TIO.readFile filename
-  let part1Result = sum . map getCalibrationValue1 . T.lines $ file
+  file <- readFile filename
+  let part1Result = sum . map getCalibrationValue1 . lines $ file
   print part1Result
 
-getCalibrationValue1 :: T.Text -> Int
+getCalibrationValue1 :: String -> Int
 getCalibrationValue1 text =
-  let digitText = T.filter isDigit text
-   in case (T.uncons digitText, T.unsnoc digitText) of
-        (Nothing, _) -> 0
-        (_, Nothing) -> 0
-        (Just (n, _), Just (_, m)) -> read [n, m]
+  let digitText = map digitToInt . filter isDigit $ text
+   in case digitText of
+        [] -> 0
+        [n] -> n * 10 + n
+        (n : ns) -> n * 10 + last ns
