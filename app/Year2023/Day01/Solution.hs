90e5e64b54408064a61b5c94bf01a495e68e82f3
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-12-06 01:48:04 +0900

    Simplify solution
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- app/Year2023/Day01/Solution.hs
+++ app/Year2023/Day01/Solution.hs
@@ -1,9 +1,6 @@
-{-# LANGUAGE ViewPatterns #-}
-
 module Year2023.Day01.Solution (solve) where
 
 import Data.Char (digitToInt, isDigit)
-import Data.List (stripPrefix)
 import Paths_advent_of_code (getDataFileName)
 
 input :: String
@@ -13,50 +10,36 @@ solve :: IO ()
 solve = do
   filename <- getDataFileName input
   file <- readFile filename
-  let part1Result = sum . map (getCalibrationValue . getDigits) . lines $ file
+  let part1Result = sum . map (getCalibrationValue . getDigits1) . lines $ file
   print part1Result
-  let part2Result = sum . map getDigits2 . lines $ file
+  let part2Result = sum . map (getCalibrationValue . getDigits2) . lines $ file
   print part2Result
 
-getDigits :: String -> [Int]
-getDigits = map digitToInt . filter isDigit
+getDigits1 :: String -> [Int]
+getDigits1 = map digitToInt . filter isDigit
+
+getDigits2 :: String -> [Int]
+getDigits2 = reverse . step []
+  where
+    step :: [Int] -> String -> [Int]
+    step acc [] = acc
+    step acc text =
+      case text of
+        ('o' : 'n' : 'e' : _cs) -> step (1 : acc) (tail text)
+        ('t' : 'w' : 'o' : _cs) -> step (2 : acc) (tail text)
+        ('t' : 'h' : 'r' : 'e' : 'e' : _cs) -> step (3 : acc) (tail text)
+        ('f' : 'o' : 'u' : 'r' : _cs) -> step (4 : acc) (tail text)
+        ('f' : 'i' : 'v' : 'e' : _cs) -> step (5 : acc) (tail text)
+        ('s' : 'i' : 'x' : _cs) -> step (6 : acc) (tail text)
+        ('s' : 'e' : 'v' : 'e' : 'n' : _cs) -> step (7 : acc) (tail text)
+        ('e' : 'i' : 'g' : 'h' : 't' : _cs) -> step (8 : acc) (tail text)
+        ('n' : 'i' : 'n' : 'e' : _cs) -> step (9 : acc) (tail text)
+        (c : cs) ->
+          if isDigit c
+            then step (digitToInt c : acc) cs
+            else step acc cs
 
 getCalibrationValue :: [Int] -> Int
 getCalibrationValue [] = 0
 getCalibrationValue [n] = n * 10 + n
 getCalibrationValue (n : ns) = n * 10 + last ns
-
-getDigits2 :: String -> Int
-getDigits2 text = getFirstDigit text * 10 + (getSecondDigit . reverse $ text)
-  where
-    getFirstDigit :: String -> Int
-    getFirstDigit [] = 0
-    getFirstDigit (stripPrefix "one" -> Just _rest) = 1
-    getFirstDigit (stripPrefix "two" -> Just _rest) = 2
-    getFirstDigit (stripPrefix "three" -> Just _rest) = 3
-    getFirstDigit (stripPrefix "four" -> Just _rest) = 4
-    getFirstDigit (stripPrefix "five" -> Just _rest) = 5
-    getFirstDigit (stripPrefix "six" -> Just _rest) = 6
-    getFirstDigit (stripPrefix "seven" -> Just _rest) = 7
-    getFirstDigit (stripPrefix "eight" -> Just _rest) = 8
-    getFirstDigit (stripPrefix "nine" -> Just _rest) = 9
-    getFirstDigit (c : cs) =
-      if isDigit c
-        then digitToInt c
-        else getFirstDigit cs
-
-    getSecondDigit :: String -> Int
-    getSecondDigit [] = 0
-    getSecondDigit (stripPrefix "eno" -> Just _rest) = 1
-    getSecondDigit (stripPrefix "owt" -> Just _rest) = 2
-    getSecondDigit (stripPrefix "eerht" -> Just _rest) = 3
-    getSecondDigit (stripPrefix "ruof" -> Just _rest) = 4
-    getSecondDigit (stripPrefix "evif" -> Just _rest) = 5
-    getSecondDigit (stripPrefix "xis" -> Just _rest) = 6
-    getSecondDigit (stripPrefix "neves" -> Just _rest) = 7
-    getSecondDigit (stripPrefix "thgie" -> Just _rest) = 8
-    getSecondDigit (stripPrefix "enin" -> Just _rest) = 9
-    getSecondDigit (c : cs) =
-      if isDigit c
-        then digitToInt c
-        else getSecondDigit cs
