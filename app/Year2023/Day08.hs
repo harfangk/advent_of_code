4ec0d054d845ee0ece013556ead231f9f659864c
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-12-21 00:20:36 +0900

    Day 8 part 2 solution
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- app/Year2023/Day08.hs
+++ app/Year2023/Day08.hs
@@ -3,8 +3,9 @@
 module Year2023.Day08 (solve) where
 
 import Control.Monad (void)
-import Data.Map (Map)
-import Data.Map qualified as Map (fromList, (!))
+import Data.List qualified as List (filter)
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map (fromList, keys, (!))
 import Data.Void (Void)
 import Paths_advent_of_code (getDataFileName)
 import Text.Megaparsec (Parsec, choice, parse, sepBy1, some)
@@ -17,30 +18,40 @@ solve :: IO ()
 solve = do
   filename <- getDataFileName input
   file <- readFile filename
-  let part1Data = parse pData "" file
-  case part1Data of
-    Right part1Data' ->
-      print ("Day 8 Part 1 answer: " ++ show (solve' part1Data'))
-    Left _ -> print "Day 8 Part 1 error parsing file"
+  let day08Input = parse pData "" file
+  case day08Input of
+    Right day08Input' -> do
+      print ("Day 8 Part 1 answer: " ++ show (solvePart1 day08Input'))
+      print ("Day 8 Part 2 answer: " ++ show (solvePart2 day08Input'))
+    Left _ -> print "Day 8 error parsing file"
 
 data Instruction = L | R deriving stock (Show)
 
 type Parser = Parsec Void String
 
-type Graph = Map String (String, String)
+type Graph = Map Node (Node, Node)
 
-type Node = String
+data Node = Node Char Char Char deriving stock (Eq, Ord, Show)
 
-solve' :: ([Instruction], Graph) -> Int
-solve' (instructions, graph) = step 0 "AAA" (cycle instructions) graph
+solvePart1 :: ([Instruction], Graph) -> Int
+solvePart1 (instructions, graph) = step 0 (Node 'A' 'A' 'A') (cycle instructions) graph
   where
     step :: Int -> Node -> [Instruction] -> Graph -> Int
-    step count "ZZZ" _ _ = count
-    step count currentNode (i : is) graph' = step (count + 1) (getNextNode currentNode i graph') is graph
+    step count (Node 'Z' 'Z' 'Z') _ _ = count
+    step count currentNode (i : is) graph' = step (count + 1) (getNextNode i graph' currentNode) is graph
     step count _ _ _ = count
 
-getNextNode :: Node -> Instruction -> Graph -> Node
-getNextNode currentNode instruction graph =
+solvePart2 :: ([Instruction], Graph) -> Int
+solvePart2 (instructions, graph) = foldl1 lcm (map (findIndividualLoop 0 (cycle instructions) graph) initialNodes)
+  where
+    initialNodes :: [Node] = List.filter (\(Node _ _ c) -> c == 'A') (Map.keys graph)
+    findIndividualLoop :: Int -> [Instruction] -> Graph -> Node -> Int
+    findIndividualLoop count _ _ (Node _ _ 'Z') = count
+    findIndividualLoop count (i : is) graph' currentNode = findIndividualLoop (count + 1) is graph (getNextNode i graph' currentNode)
+    findIndividualLoop count _ _ _ = count
+
+getNextNode :: Instruction -> Graph -> Node -> Node
+getNextNode instruction graph currentNode =
   case instruction of
     L -> fst currentEdge
     R -> snd currentEdge
@@ -63,8 +74,11 @@ pGraph = do
 pInstruction :: Parser Instruction
 pInstruction = choice [L <$ char 'L', R <$ char 'R']
 
-pNode :: Parser String
-pNode = some upperChar
+pNode :: Parser Node
+pNode = do
+  c1 <- upperChar
+  c2 <- upperChar
+  Node c1 c2 <$> upperChar
 
 pEdge :: Parser (Node, (Node, Node))
 pEdge = do
